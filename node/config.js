a281a004768d2cb13db34ec4a513a80d3e41d0f5
Andy Kuan <andy952737@gmail.com>
2023-11-07 12:29:02 +0800

    update .gitignore

--- node/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = { 
-  user: 'db_user',
-  host: 'localhost',          // 通常是 'localhost' 或数据库服务器的 IP 地址
-  database: 'database_name',
-  password: 'password',
-  port: 5432,                 // 預設 PostgreSQL 端口
-};  
